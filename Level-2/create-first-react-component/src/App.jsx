181a7d764bedbdd2b9920382a5741a28fd62dc84
Aparna Raha <aparnaraha2912@gmail.com>
2023-11-30 07:12:18 +0000

    Level-2 Create React Component Completed

---
+++ Level-2/create-first-react-component/src/App.jsx
@@ -0,0 +1,20 @@
+import {KgButton} from "./KgButton";
+import Hello from "./Hello";
+import Random from "./Random";
+
+function App() {
+  return <div>
+    <h1>
+      This is the best React course
+    </h1>
+    <br />
+    <Hello />
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+  </div>
+}
+
+export default App;
